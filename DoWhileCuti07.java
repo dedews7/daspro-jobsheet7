6033dda6f0b33d9fc2ab04d3a67ffdb1ef94ad5e
dedews7 <144302605+dedews7@users.noreply.github.com>
2023-10-12 15:02:59 +0700

    Add files via upload

---
+++ DoWhileCuti07.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DoWhileCuti07 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = scan.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = scan.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = scan.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti anda sekarang: " + jatahCuti + " hari");
+                } else {
+                    continue;
+                }
+            } else if (konfirmasi.equalsIgnoreCase("t")) {
+               
+            }
+        } while (jatahCuti > 0);
+
+        scan.close();
+    }
+}
---
+++ ForKelipatan07Modifikasi.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ForKelipatan07Modifikasi {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int kelipatan, jumlah, counter;
+        jumlah=0;
+        counter=0; 
+       
+
+System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+kelipatan = scan.nextInt();
+for (int i = 1; i <= 50; i++) {
+    if (i % kelipatan == 0) {
+        jumlah += i;
+        counter++;
+       
+    }
+}
+
+double RataRataKelipatan = (double)jumlah / counter;
+    
+System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+System.out.println("Rata-rata: " + RataRataKelipatan);
+
+    }
+
+    
+}
